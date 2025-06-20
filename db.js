1846f5da8050cd4570025f32e0a9397fd4c93ab9
JigyasuT <jigyasuti@gmail.com>
2025-06-20 15:20:17 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
 const mongoose=require('mongoose')
-
+require('dotenv').config();
 //define the MongoDB connection url
 
-const mongoURL='mongodb://localhost:27017/Hotels'
+// const mongoURL='mongodb://localhost:27017/Hotels'
+// const mongoURL = 'mongodb+srv://jigyasuti:jigyasutiwari2005@cluster100.9vesa6m.mongodb.net/'
+// const mongoURL=process.env.MONGODB_URL_LOCAL;
+const mongoURL=process.env.MONGODB_URL;
 
 // set up MongoDB connection
 mongoose.connect(mongoURL,{
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
-
+require('dotenv').config()
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); //req.body
 
-
+const PORT=process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Wlcome to our hotels");
@@ -20,6 +20,9 @@ const menuItemRoutes=require('./routes/menuItemRoutes')
 
 app.use('/person',personRoutes);
 app.use('/menu',menuItemRoutes);
+
+
+
 app.listen(3000, () => {
   console.log("server is running on the port 3000");
 });
